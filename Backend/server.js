a193aa8334146fad7ef8deff3d8ad111a4be5ba7
Joyita Bhattacharjee <joyitabhattacharya16@gmail.com>
2026-02-19 01:25:51 +0530

    Allow deployed frontend in CORS

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://asknovafrontend-yb4w.onrender.com"
+  ],
+  credentials: true
+}));
+
 
 // Routes
 app.use("/api/auth", authRoutes);
